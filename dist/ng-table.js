fec1c5731df0abcd0a90b3fd5f4ea124d1d272da
Marc Lough <marc@ahead4.com>
2016-08-08 10:08:24 +0100

    Replace title with heading in dist JS for (temporary for now).

--- dist/ng-table.js
+++ dist/ng-table.js
@@ -467,8 +467,8 @@
                 headerTitle: createGetterSetter(''),
                 sortable: createGetterSetter(false),
                 show: createGetterSetter(true),
-                title: createGetterSetter(''),
-                titleAlt: createGetterSetter('')
+                heading: createGetterSetter(''),
+                headingAlt: createGetterSetter('')
             };
         }
 
@@ -1632,17 +1632,17 @@
                             };
                             return getter;
                         };
-                        var titleExpr = getAttrValue('title-alt') || getAttrValue('title');
-                        if (titleExpr){
-                            el.attr('data-title-text', '{{' + titleExpr + '}}'); // this used in responsive table
+                        var headingExpr = getAttrValue('heading-alt') || getAttrValue('heading');
+                        if (headingExpr){
+                            el.attr('data-heading-text', '{{' + headingExpr + '}}'); // this used in responsive table
                         }
                         // NOTE TO MAINTAINERS: if you add extra fields to a $column be sure to extend ngTableColumn with
                         // a corresponding "safe" default
                         columns.push({
                             id: i++,
-                            title: parsedAttribute('title'),
-                            titleAlt: parsedAttribute('title-alt'),
-                            headerTitle: parsedAttribute('header-title'),
+                            heading: parsedAttribute('heading'),
+                            headingAlt: parsedAttribute('heading-alt'),
+                            headerTitle: parsedAttribute('header-heading'),
                             sortable: parsedAttribute('sortable'),
                             'class': parsedAttribute('header-class'),
                             filter: parsedAttribute('filter'),
@@ -1721,9 +1721,9 @@
                     };
 
                     // this used in responsive table
-                    var titleExpr = getAttrValue('title');
-                    if (!titleExpr){
-                        el.attr('data-title-text', '{{$columns[$index].titleAlt(this) || $columns[$index].title(this)}}');
+                    var headingExpr = getAttrValue('heading');
+                    if (!headingExpr){
+                        el.attr('data-heading-text', '{{$columns[$index].headingAlt(this) || $columns[$index].heading(this)}}');
                     }
                     var showExpr = el.attr('ng-if');
                     if (!showExpr){
@@ -1962,7 +1962,7 @@
         }
 
         function getGroupTitle(group){
-            return angular.isFunction(group) ? group.title : group.title($scope);
+            return angular.isFunction(group) ? group.heading : group.heading($scope);
         }
 
         function getGroupables(){
@@ -2006,13 +2006,13 @@
             if (angular.isFunction(group)) {
                 groupFns = [group];
                 $scope.$selGroup = group;
-                $scope.$selGroupTitle = group.title;
+                $scope.$selGroupTitle = group.heading;
             } else {
                 // note: currently only one group is implemented
                 var groupKey = Object.keys(group || {})[0];
                 var groupedColumn = findGroupColumn(groupKey);
                 if (groupedColumn) {
-                    $scope.$selGroupTitle = groupedColumn.title($scope);
+                    $scope.$selGroupTitle = groupedColumn.heading($scope);
                     $scope.$selGroup = groupKey;
                     if (groupedColumn.show.assign) {
                         groupedColumn.show.assign($scope, false);
@@ -2181,7 +2181,7 @@
         function bindDataSource(){
             getSelectListData($column).then(function(data){
                 if (data && !hasEmptyOption(data)){
-                    data.unshift({ id: '', title: ''});
+                    data.unshift({ id: '', heading: ''});
                 }
                 data = data || [];
                 $scope.$selectData = data;
@@ -2208,15 +2208,15 @@
 })();
 
 angular.module('ngTable').run(['$templateCache', function ($templateCache) {
-	$templateCache.put('ng-table/filterRow.html', '<tr ng-show="show_filter" class="ng-table-filters"> <th data-title-text="{{$column.titleAlt(this) || $column.title(this)}}" ng-repeat="$column in $columns" ng-if="$column.show(this)" class="filter {{$column.class(this)}}" ng-class="params.settings().filterOptions.filterLayout===\'horizontal\' ? \'filter-horizontal\' : \'\'"> <div ng-repeat="(name, filter) in $column.filter(this)" ng-include="config.getTemplateUrl(filter)" class="filter-cell" ng-class="[getFilterCellCss($column.filter(this), params.settings().filterOptions.filterLayout), $last ? \'last\' : \'\']"> </div> </th> </tr> ');
+	$templateCache.put('ng-table/filterRow.html', '<tr ng-show="show_filter" class="ng-table-filters"> <th data-heading-text="{{$column.headingAlt(this) || $column.heading(this)}}" ng-repeat="$column in $columns" ng-if="$column.show(this)" class="filter {{$column.class(this)}}" ng-class="params.settings().filterOptions.filterLayout===\'horizontal\' ? \'filter-horizontal\' : \'\'"> <div ng-repeat="(name, filter) in $column.filter(this)" ng-include="config.getTemplateUrl(filter)" class="filter-cell" ng-class="[getFilterCellCss($column.filter(this), params.settings().filterOptions.filterLayout), $last ? \'last\' : \'\']"> </div> </th> </tr> ');
 	$templateCache.put('ng-table/filters/number.html', '<input type="number" name="{{name}}" ng-disabled="$filterRow.disabled" ng-model="params.filter()[name]" class="input-filter form-control" placeholder="{{getFilterPlaceholderValue(filter, name)}}"/> ');
-	$templateCache.put('ng-table/filters/select-multiple.html', '<select ng-options="data.id as data.title for data in $column.data" ng-disabled="$filterRow.disabled" multiple ng-multiple="true" ng-model="params.filter()[name]" class="filter filter-select-multiple form-control" name="{{name}}"> </select> ');
-	$templateCache.put('ng-table/filters/select.html', '<select ng-options="data.id as data.title for data in $selectData" ng-table-select-filter-ds="$column" ng-disabled="$filterRow.disabled" ng-model="params.filter()[name]" class="filter filter-select form-control" name="{{name}}"> <option style="display:none" value=""></option> </select> ');
+	$templateCache.put('ng-table/filters/select-multiple.html', '<select ng-options="data.id as data.heading for data in $column.data" ng-disabled="$filterRow.disabled" multiple ng-multiple="true" ng-model="params.filter()[name]" class="filter filter-select-multiple form-control" name="{{name}}"> </select> ');
+	$templateCache.put('ng-table/filters/select.html', '<select ng-options="data.id as data.heading for data in $selectData" ng-table-select-filter-ds="$column" ng-disabled="$filterRow.disabled" ng-model="params.filter()[name]" class="filter filter-select form-control" name="{{name}}"> <option style="display:none" value=""></option> </select> ');
 	$templateCache.put('ng-table/filters/text.html', '<input type="text" name="{{name}}" ng-disabled="$filterRow.disabled" ng-model="params.filter()[name]" class="input-filter form-control" placeholder="{{getFilterPlaceholderValue(filter, name)}}"/> ');
 	$templateCache.put('ng-table/groupRow.html', '<tr ng-if="params.hasGroup()" ng-show="$groupRow.show" class="ng-table-group-header"> <th colspan="{{getVisibleColumns().length}}" class="sortable" ng-class="{ \'sort-asc\': params.hasGroup($selGroup, \'asc\'), \'sort-desc\':params.hasGroup($selGroup, \'desc\') }"> <a href="" ng-click="isSelectorOpen=!isSelectorOpen" class="ng-table-group-selector"> <strong class="sort-indicator">{{$selGroupTitle}}</strong> <button class="btn btn-default btn-xs ng-table-group-close" ng-click="$groupRow.show=false; $event.preventDefault(); $event.stopPropagation();"> <span class="glyphicon glyphicon-remove"></span> </button> <button class="btn btn-default btn-xs ng-table-group-toggle" ng-click="toggleDetail(); $event.preventDefault(); $event.stopPropagation();"> <span class="glyphicon" ng-class="{ \'glyphicon-resize-small\': params.settings().groupOptions.isExpanded, \'glyphicon-resize-full\': !params.settings().groupOptions.isExpanded }"></span> </button> </a> <div class="list-group" ng-if="isSelectorOpen"> <a href="" class="list-group-item" ng-repeat="group in getGroupables()" ng-click="groupBy(group)"> <strong>{{ getGroupTitle(group)}}</strong> <strong ng-class="isSelectedGroup(group) && \'sort-indicator\'"></strong> </a> </div> </th> </tr> ');
 	$templateCache.put('ng-table/header.html', '<ng-table-group-row></ng-table-group-row> <ng-table-sorter-row></ng-table-sorter-row> <ng-table-filter-row></ng-table-filter-row> ');
 	$templateCache.put('ng-table/pager.html', '<div class="ng-cloak ng-table-pager" ng-if="params.data.length"> <div ng-if="params.settings().counts.length" class="ng-table-counts btn-group pull-right"> <button ng-repeat="count in params.settings().counts" type="button" ng-class="{\'active\':params.count()==count}" ng-click="params.count(count)" class="btn btn-default"> <span ng-bind="count"></span> </button> </div> <ul ng-if="pages.length" class="pagination ng-table-pagination"> <li ng-class="{\'disabled\': !page.active && !page.current, \'active\': page.current}" ng-repeat="page in pages" ng-switch="page.type"> <a ng-switch-when="prev" ng-click="params.page(page.number)" href="">&laquo;</a> <a ng-switch-when="first" ng-click="params.page(page.number)" href=""><span ng-bind="page.number"></span></a> <a ng-switch-when="page" ng-click="params.page(page.number)" href=""><span ng-bind="page.number"></span></a> <a ng-switch-when="more" ng-click="params.page(page.number)" href="">&#8230;</a> <a ng-switch-when="last" ng-click="params.page(page.number)" href=""><span ng-bind="page.number"></span></a> <a ng-switch-when="next" ng-click="params.page(page.number)" href="">&raquo;</a> </li> </ul> </div> ');
-	$templateCache.put('ng-table/sorterRow.html', '<tr class="ng-table-sort-header"> <th title="{{$column.headerTitle(this)}}" ng-repeat="$column in $columns" ng-class="{ \'sortable\': $column.sortable(this), \'sort-asc\': params.sorting()[$column.sortable(this)]==\'asc\', \'sort-desc\': params.sorting()[$column.sortable(this)]==\'desc\' }" ng-click="sortBy($column, $event)" ng-if="$column.show(this)" ng-init="template=$column.headerTemplateURL(this)" class="header {{$column.class(this)}}"> <div ng-if="!template" class="ng-table-header" ng-class="{\'sort-indicator\': params.settings().sortingIndicator==\'div\'}"> <span ng-bind="$column.title(this)" ng-class="{\'sort-indicator\': params.settings().sortingIndicator==\'span\'}"></span> </div> <div ng-if="template" ng-include="template"></div> </th> </tr> ');
+	$templateCache.put('ng-table/sorterRow.html', '<tr class="ng-table-sort-header"> <th heading="{{$column.headerTitle(this)}}" ng-repeat="$column in $columns" ng-class="{ \'sortable\': $column.sortable(this), \'sort-asc\': params.sorting()[$column.sortable(this)]==\'asc\', \'sort-desc\': params.sorting()[$column.sortable(this)]==\'desc\' }" ng-click="sortBy($column, $event)" ng-if="$column.show(this)" ng-init="template=$column.headerTemplateURL(this)" class="header {{$column.class(this)}}"> <div ng-if="!template" class="ng-table-header" ng-class="{\'sort-indicator\': params.settings().sortingIndicator==\'div\'}"> <span ng-bind="$column.heading(this)" ng-class="{\'sort-indicator\': params.settings().sortingIndicator==\'span\'}"></span> </div> <div ng-if="template" ng-include="template"></div> </th> </tr> ');
 }]);
     return angular.module('ngTable');
 }));
